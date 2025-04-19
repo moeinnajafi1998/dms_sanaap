808153edbb2480133c7b0d7c8aa62809b6eceedf
Moeinnajafi1998 <moeinnajafi1996@gmail.com>
2025-04-19 11:56:34 +0330

    DocumentAdmin

--- documents_app/admin.py
+++ documents_app/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Document
 
-admin.site.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created_at', 'updated_at')  
+    list_filter = ('owner', 'created_at')  
+    search_fields = ('title', 'description')  
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')  
+    fields = ('title', 'description', 'file', 'owner')  
+    save_on_top = True  
+
+admin.site.register(Document, DocumentAdmin)
+
